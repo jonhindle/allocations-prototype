225afb611e65641bc028976d50cb0a59a3543df0
jonhindle <jon.hindle@digital.justice.gov.uk>
2020-11-10 16:01:11 +0000

    initial-screens

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,13 @@
+const { get } = require('browser-sync')
 const express = require('express')
 const router = express.Router()
+const { serviceUsers, probationPractitioners } = require('./views/allocations/0/data.json')
 
 // Add your routes here - above the module.exports line
+
+router.get('/allocations/0/*', ({ path, query, originalUrl }, res) => {
+  console.log(query)
+  res.render(path.substring(1), { query, serviceUsers, probationPractitioners })
+})
 
 module.exports = router
